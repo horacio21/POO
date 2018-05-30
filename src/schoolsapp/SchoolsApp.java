0a097b153613e0435d1bd06dd77368625c968602
Horacio Ureña <20121346@ce.pucmm.edu.do>
2018-05-30 00:10:18 -0400

    Initial

---
+++ src/schoolsapp/SchoolsApp.java
@@ -0,0 +1,16 @@
+package schoolsapp;
+
+/**
+ *
+ * @author horac
+ */
+public class SchoolsApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
